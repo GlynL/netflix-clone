4fa75df04e6bcd34166bc1bc64a4f8120d9f7cca
Glyn L <glyn_92@hotmail.com>
2019-01-17 17:02:27 +0700

    added navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,78 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faBell,
+  faUsers,
+  faSortDown
+} from "@fortawesome/free-solid-svg-icons";
+
+import styled from "styled-components";
+
+const Link = styled.a`
+  height: 6rem;
+  max-width: 13rem;
+  color: #e5e5e5;
+  font-size: 1.6rem;
+  padding-left: 2rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+
+  &:active,
+  &:focus,
+  &:hover {
+    color: #b3b3b3;
+  }
+`;
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  background: black;
+`;
+
+const Container = styled.div`
+  margin-left: auto;
+  padding-right: 2rem;
+  display: flex;
+`;
 
 const Header = () => {
   return (
     <section>
-      <nav>
-        <a href="/">Netflix</a>
-      </nav>
+      <Nav>
+        <Link href="/">
+          <Image src="/logo.jpg" alt="Netflix" />
+        </Link>
+        <Link>Home</Link>
+        <Link>TV Shows</Link>
+        <Link>Movies</Link>
+        <Link>Recently Added</Link>
+        <Link>My List</Link>
+        <Container>
+          <Link>
+            <FontAwesomeIcon icon={faSearch} />
+          </Link>
+          <Link>Kids</Link>
+          <Link>
+            <FontAwesomeIcon icon={faBell} />
+          </Link>
+          <div>
+            <Link>
+              <FontAwesomeIcon icon={faUsers} />
+              <FontAwesomeIcon
+                icon={faSortDown}
+                style={{ "padding-left": "1rem" }}
+              />
+            </Link>
+          </div>
+        </Container>
+      </Nav>
     </section>
   );
 };
