205a5574d0632da79b4ced8da35bd908aee065b7
Glyn L <glyn_92@hotmail.com>
2019-01-19 10:32:04 +0700

    refactored movies/genres

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import MoviesList from "./MoviesList";
 import { API_KEY } from "../variables.js";
+import { reject } from "q";
 
 const List = styled.ul`
   list-style: none;
@@ -10,22 +11,47 @@ const List = styled.ul`
 
 const Genres = ({ genres }) => {
   const [popGenres, setPopGenres] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchTrending = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
+      );
+      if (!response.ok) throw new Error();
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      setError("Error fetching data. Please refresh and try again.");
+    }
+  };
 
   const fetchMovies = async () => {
-    const urls = genres.map(
-      genre =>
-        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${
-          genre.id
-        }`
-    );
-    const responses = await Promise.all(urls.map(url => fetch(url)));
-    const data = await Promise.all(responses.map(response => response.json()));
-    const genreData = data.reduce((acc, curr, i) => {
-      const genreResults = { ...genres[i], results: curr.results };
-      return [...acc, genreResults];
-    }, []);
-
-    setPopGenres(genreData);
+    try {
+      const urls = genres.map(
+        genre =>
+          `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${
+            genre.id
+          }`
+      );
+      const responses = await Promise.all(urls.map(url => fetch(url)));
+      const data = await Promise.all(
+        responses.map(response => response.json())
+      );
+      const trendingData = await fetchTrending();
+      const trending = {
+        name: "Trending Now",
+        id: 0,
+        results: trendingData.results
+      };
+      const genreData = data.reduce((acc, curr, i) => {
+        const genreResults = { ...genres[i], results: curr.results };
+        return [...acc, genreResults];
+      }, []);
+      setPopGenres([trending, ...genreData]);
+    } catch (err) {
+      setError("Error fetching data. Please refresh and try again.");
+    }
   };
 
   useEffect(() => {
@@ -41,6 +67,7 @@ const Genres = ({ genres }) => {
   if (popGenres.length === 0) return "genres loading";
   return (
     <section>
+      {error && <div>{error}</div>}
       <List>{list}</List>
     </section>
   );
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,51 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { API_KEY, genres } from "../variables";
+import { genres } from "../variables";
 
 import Genres from "./Genres";
-import MoviesList from "./MoviesList";
 
 // add in duplicate check
 const randomGenres = (number = 5) => {
   let randomised = [];
-  for (let i = 0; i < number; i++) {
+  const rndNums = [];
+
+  const getRandomNum = () => {
     const rnd = Math.floor(Math.random() * genres.length);
-    randomised.push(genres[rnd]);
+    if (rndNums.includes(rnd)) {
+      getRandomNum();
+    } else {
+      rndNums.push(rnd);
+      randomised.push(genres[rnd]);
+    }
+  };
+
+  for (let i = 0; i < number; i++) {
+    getRandomNum();
   }
+
   return randomised;
 };
 
 const Movies = () => {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
   const [genres, setGenres] = useState([]);
-
-  const fetchMovies = async () => {
-    try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`
-      );
-      if (!response.ok) throw new Error();
-      const data = await response.json();
-      setMovies(data.results);
-    } catch (err) {
-      setError("Error fetching data. Please refresh and try again.");
-    }
-  };
 
   const generateRandomGenres = (number = 5) => {
     setGenres(randomGenres(number));
   };
 
   useEffect(() => {
-    fetchMovies();
     generateRandomGenres(2);
   }, []);
 
-  if (movies.lenght === 0 || genres.length === 0) return "loading";
+  if (genres.length === 0) return "loading";
   return (
     <div>
-      {error && <div>{error}</div>}
-      <MoviesList movies={movies} genre={"Trending Now"} />
       <Genres genres={genres} />
     </div>
   );
