bf4df342f9c3b65d476c7d2110935df30619a564
Glyn L <glynlewington@gmail.com>
2019-02-06 13:28:38 +0700

    responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,39 +10,32 @@ import styled from "styled-components";
 
 const Icon = styled(FontAwesomeIcon)`
   font-size: 3rem;
-  margin-right: 1rem;
+  margin-right: 1.5rem;
 `;
 
 const Container = styled.div`
+  margin: 0 auto;
+  padding-left: 1rem;
+  max-width: 600px;
   color: grey;
   font-size: 1.3rem;
-  display: grid;
-  grid-template-columns: repeat(4, 200px);
-  grid-template-rows: repeat(7, 1fr);
-  justify-content: center;
-  align-items: center;
 `;
 
-const Icons = styled.div`
-  // display: flex;
-  // justify-content: flex-start;
-  grid-column: span 4;
-`;
+const Icons = styled.div``;
 
 const Btn = styled.button`
   background: none;
   border: 1px solid grey;
   color: inherit;
-  grid-column: span 4;
-  width: 100px;
   padding: 0.5rem;
 `;
 
-// const List = styled.ul`
-// display: grid;
-// grid-template-columns: repeat(4, 200px);
-// grid-template-rows: repeat(5, 40px);
-// `;
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  padding: 0;
+  grid-gap: 2fr;
+`;
 
 const Footer = () => {
   return (
@@ -53,28 +46,26 @@ const Footer = () => {
         <Icon icon={faTwitter} />
         <Icon icon={faYoutube} />
       </Icons>
-      {/* <List> */}
-      <p>Audio and Subtitles</p>
-      <p>Audio Description</p>
-      <p>Help Center</p>
-      <p>Gift Cards</p>
-      <p>Media Center</p>
-      <p>Investor Relations</p>
-      <p>Jobs</p>
-      <p>Terms of Use</p>
-      <p>Privacy</p>
-      <p>Legal Notices</p>
-      <p>Cookie Preferences</p>
-      <p>Corporate Information</p>
-      <p>Contact Us</p>
-      <div />
-      <div />
-      <div />
-      {/*</List> */}
+      <List>
+        <p>Audio and Subtitles</p>
+        <p>Audio Description</p>
+        <p>Help Center</p>
+        <p>Gift Cards</p>
+        <p>Media Center</p>
+        <p>Investor Relations</p>
+        <p>Jobs</p>
+        <p>Terms of Use</p>
+        <p>Privacy</p>
+        <p>Legal Notices</p>
+        <p>Cookie Preferences</p>
+        <p>Corporate Information</p>
+        <p>Contact Us</p>
+        <div />
+        <div />
+        <div />
+      </List>
       <Btn>Service Code</Btn>
-      <p style={{ gridColumn: "span 2" }}>
-        © 1997-2019 Netflix, Inc. i-0438ea75f2b133c22b
-      </p>
+      <p>© 1997-2019 Netflix, Inc. i-0438ea75f2b133c22b</p>
     </Container>
   );
 };
